ba84abafecd89c770b6aed406446e6ee4f75f747
LinnetteCapul <linnette.capul@gmail.com>
2022-04-02 12:20:43 -0700

    added inquirer prompt for main menu

--- server.js
+++ server.js
@@ -12,3 +12,43 @@ const db = mysql.createConnection(
     console.log(`Connected to the employee database.`)
   );
 
+function mainMenu() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "menuChoices",
+                message: "What would you like to do?",
+                choices: [
+                    "View all departments",
+                    "View all roles",
+                    "View all employees",
+                    "Add a department",
+                    "Add a role",
+                    "Add an employee",
+                    "Update an employee role",
+                    "Quit"
+                ],
+            },
+        ])
+        .then((choice) => {
+            switch (choice.menuChoices) {
+                case "View all departments": 
+                    return viewAllDepts();
+                case "View all roles":
+                    return viewAllRoles();
+                case "View all employees":
+                    return viewAllEmployees();
+                case "Add a department":
+                    return addDept();
+                case "Add a role":
+                    return addRole();
+                case "Add an employee":
+                    return addEmployee();
+                case "Update an employee role":
+                    return updateEmployRole();
+                default:
+                    process.exit()
+            }
+        });
+}
